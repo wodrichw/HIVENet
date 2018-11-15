2680337a88a0cfec48cf246627d8187c3b8d2ff1
wodrichw <wodrichw@oregonstate.edu>
2018-11-14 21:31:44 -0800

    added pip3 install to createVirtualEnv.sh

--- scripts/createVirtualEnv.sh
+++ scripts/createVirtualEnv.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+sudo apt -y install python3
+
+sudo apt -y update
+
+sudo apt -y install python3-pip
+
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+python3 get-pip.py --user
+
+rm get-pip.py
+
 sudo pip3 install -U virtualenv
 
 virtualenv --system-site-packages -p python3 ./tensorflow
