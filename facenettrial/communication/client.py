7557fbd5d92e1c6edd4d8fc657768fa40c961a81
William Wodrich <wodrichw@oregonstate.edu>
2019-02-13 14:36:44 -0800

    successful file upload

--- facenettrial/communication/client.py
+++ facenettrial/communication/client.py
@@ -7,7 +7,8 @@ from os import listdir
 
 
 # api-endpoint 
-URL = "http://127.0.0.1"
+URL = "http://127.0.0.1:5000"
+
 
 fin = open('dev/cesilia.tar.gz', 'rb')
 files = {'file': fin}
--- facenettrial/communication/server.py
+++ facenettrial/communication/server.py
@@ -1,36 +1,17 @@
-# http://flask.pocoo.org/docs/patterns/fileuploads/
 import os
 from flask import Flask, request, redirect, url_for, send_from_directory
 
-UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'dev/'
-
-def allowed_file(filename):
-  # this has changed from the original example because the original did not work for me
-    return filename[-3:].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
-        if file and allowed_file(file.filename):
-            print '**found file', file.filename
+        if file: 
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-            # for browser, add 'redirect' function on top of 'url_for'
-            return url_for('uploaded_file',
-                                    filename=file.filename)
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+            return url_for('uploaded_file', filename=file.filename)
+    return "file upload not successful"
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
