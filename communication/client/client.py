e149d411aa9d91ea2845cb3b56a9925eb516d774
William Wodrich <wodrichw@oregonstate.edu>
2019-04-05 17:46:12 -0700

    pulled working version from client.py

--- communication/client/client.py
+++ communication/client/client.py
@@ -5,12 +5,74 @@ import json
 from flask import jsonify
 import os
 
-RD = os.path.dirname(os.path.realpath(__file__))
+URL = "http://10.248.170.135:5000"
+ROOTDIR = os.path.dirname(os.path.realpath(__file__))
+
+# ######################################################
+# # API calls for Alpha version
+# ####################################################@#
+
+
+# def addName(newName):
+#     with open(ROOTDIR+'assets/newNames.txt', 'a+') as nf:
+#         nf.writelines([newName])
+
+# def sendNewPhotos(url=URL):
+#     tarCmd = ROOTDIR+'/scripts/compressPhotos.sh'
+#     subprocess.call([tarCmd])
+#     fin = open(ROOTDIR+'/assets/tarPhotos/newNamesData.tar.gz', 'rb')
+#     files = {'file': fin}
+#     try:
+#         r = requests.post(URL+'/training-data', files=files)
+#         print(r.text)
+#     finally:
+#         fin.close()
+
+# def sendNames(url=URL):
+#     fin = open(ROOTDIR+'/assets/newNames.txt', 'r')
+#     files = {'file': fin}
+#     try:
+#         r = requests.post(URL+'/names', files=files)
+#         print(r.text)
+#     finally:
+#         fin.close()
+
+# def sendClassifier(url=URL):
+#     fin = open(os.path.realpath('./classifier.pkl'))
+#     files = {'file': fin}
+#     try:
+#         r = requests.post(URL+'/classifier', files=files)
+#         print(r.text)
+#     finally:
+#         fin.close()
+
+# def sendAll(url=URL):
+#     classifierF = open(ROOTDIR+'/assets/newNames.txt', 'r')
+
+#     namesF = open(ROOTDIR+'/assets/newNames.txt', 'r')
+
+#     tarCmd = ROOTDIR+'/scripts/compressPhotos.sh'
+#     subprocess.call([tarCmd])
+#     trainingDataF = open(ROOTDIR+'/assets/tarPhotos/newNamesData.tar.gz', 'rb')
+
+#     files = {'classifier': classifierF, 'names': namesF, 'trainingData': trainingDataF}
+
+#     try:
+#         r = requests.post(URL+'/sync-all', files=files)
+#         print(r.text)
+#     finally:
+#         classifierF.close()
+#         namesF.close()
+#         trainingDataF.close()
+
+######################################################
+# AP Calling for Beta version
+####################################################@#
 
 def sendClassifier(ip):
     try: 
-        classifierF = open(RD+'/assets/classifier.pkl','r')
-        namesF = open(RD+'/assets/names.txt','r')
+        classifierF = open(ROOTDIR+'/assets/classifier.pkl','r')
+        namesF = open(ROOTDIR+'/assets/names.txt','r')
         try:
             files = {"classifier":classifierF,"names":namesF}
             r = requests.post('http://'+ip+':5000/beta/update_classifier', files=files)
@@ -20,14 +82,15 @@ def sendClassifier(ip):
             classifierF.close()
             namesF.close()
     except: 
-        print("Files not found: " + RD + '/assets/names.txt or ' + RD+'/assets/classifier.pkl')
+        print("Files not found: " + ROOTDIR + '/assets/names.txt or ' + ROOTDIR+'/assets/classifier.pkl')
 def sendToEdgeDevices():
     # Get the IP of edge device
-    p = subprocess.Popen(RD+'/scripts/getIPs.sh', shell=True, stdout=subprocess.PIPE)
+    p = subprocess.Popen(ROOTDIR+'/scripts/getIPs.sh', shell=True, stdout=subprocess.PIPE)
     IPaddr = p.communicate()#.strip()
     for ip in IPaddr:
         if ip is not None and ip != '': 
             ip.rstrip()
+            if ip[-1] == '\n': ip = ip[:-1]
             print(ip)
             sendClassifier(ip)
 
--- communication/server/server.py
+++ communication/server/server.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+
 import os
 import subprocess
 from flask import Flask, request, redirect, url_for, send_from_directory
@@ -8,54 +10,6 @@ from flask import Flask, request, redirect, url_for, send_from_directory
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'assets/'
 rootDir = os.path.dirname(os.path.realpath(__file__))
-
-
-#####################################################
-# API handling for ALPHA version
-####################################################
-@app.route('/alpha/training-data', methods=['POST'])
-def uploadTrainingData():
-    handleTrainingData(request.files['file'])
-
-def handleTrainingData(file):
-    if file: 
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'tarPhotos/', file.filename))
-        tarCmd = rootDir+'/scripts/decompressPhotos.sh'
-        subprocess.call([tarCmd])
-        return file.filename+' upload successful'
-    return file.filename+" upload not successful"
-
-@app.route('/alpha/classifier', methods=['POST'])
-def uploadClassifier():
-    handleClassifier(request.files['file'])
-
-def handleClassifier(file):
-    if file: 
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-        return file.filename+'upload successful'
-    return file.filename+" upload not successful"
-
-
-@app.route('/alpha/names', methods=['POST'])
-def uploadNames():
-    handleNames(request.files['file'])
-
-def handleNames(file):
-    if file: 
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-        appendNamesCmd = rootDir + '/scripts/'
-        subprocess.call([])
-        return file.filename+'upload successful'
-    return "file upload not successful"
-
-
-
-@app.route('/alpha/sync-all', methods=['POST'])
-def syncAll():
-    handleTrainingData(request.files['trainingData'])
-    handleClassifier(request.files['classifier'])
-    handleNames(request.files['names'])
-
 
 ######################################################
 # API Handleing for Beta version
