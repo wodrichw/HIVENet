94f74ca79b3fd6422907f69d6562f33c5b43ba94
Andrew Davis <daviandr@oregonstate.edu>
2019-04-07 23:51:24 +0000

    setup

--- setup/installer.sh
+++ setup/installer.sh
@@ -7,16 +7,28 @@
 #python-tk
 
 sudo apt install python
-sudo apt install python-pip
 
-for line in $(cat requirements.txt -)
-do
-	pip install $line
-	pip3 install $line
-done
+sudo apt-get install python-pip
 
+#for line in $(cat requirements.txt -)
+#do
+#	pip install $line
+#	pip3 install $line
+#done
+
+pip install tensorflow==1.7
+pip install scipy
+pip install scikit-learn
+pip install opencv-python
+pip install h5py
+pip install matplotlib
+pip install Pillow
+pip install requests
+pip install psutil
+pip install flask
 
 #Communication
+sudo apt install net-tools
 sudo apt install openssh-server
 sudo apt install nmap
 sudo apt install python-tk
