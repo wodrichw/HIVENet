3b02014b0be31a11d4c67471d8aec99252112fe2
enrique <fernanen@oregonstate.edu>
2019-04-12 06:29:23 +0000

    cleaned setup script.

--- setup/installer.sh
+++ setup/installer.sh
@@ -1,18 +1,23 @@
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+apt install curl
+apt install gcc
+apt install openssh-server
+apt install nmap
+apt install python-tk
+apt install net-tools
+apt install python-dev 
+
 # Install openface requirements and facenet requirements
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 
-
-git clone git@github.com:davidsandberg/facenet.git
-cd facenet
 for line in $(cat requirements.txt)
 do
 	pip install $line
-	pip3 install $line
 done
 
-
-# Communication
-sudo apt install openssh-server
-sudo apt install nmap
-sudo apt install python-tk
